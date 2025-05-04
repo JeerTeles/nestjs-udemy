fe702cc7fb1d4db1eb826982bb3c48535c88bcd2
JeerTeles <jadersoncontatos@gmail.com>
2025-05-04 02:54:26 -0300

    feature: importing usersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModules } from './user/user.modules';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModules],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
