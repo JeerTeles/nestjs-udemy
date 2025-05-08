987dca5db0981658110bd7b39a0c73ea78d67a89
JeerTeles <jadersoncontatos@gmail.com>
2025-05-07 21:47:06 -0300

    feature: create auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
     imports: [JwtModule.register({
         secret: `w.P8h7,SmleCQdFyz5X;WUr-,WP7ClhV`
-    })]
+        }),
+        UserModule,
+        PrismaModule
+    ],
+    controllers: [AuthController]
 })
-export class AppModule {
+export class AuthModule {
 
 }
