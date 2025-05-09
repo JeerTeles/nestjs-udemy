80587b3a60d1ec97d44675a5faeeaef2d49a1e37
JeerTeles <jadersoncontatos@gmail.com>
2025-05-09 03:36:20 -0300

    feat: placing the decorator @Roles(Role.Admin) in the UserController globally, for all routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,6 +10,7 @@ import { Role } from "src/enums/role.enum";
 import { RoleGuard } from "src/guards/role.guard";
 import { AuthGuard } from "src/guards/auth.guard";
 
+@Roles(Role.Admin)
 @UseGuards(AuthGuard, RoleGuard)
 @UseInterceptors(LogInterceptor)
 @Controller('users')
@@ -17,38 +18,33 @@ export class UserController {
 
     constructor(private readonly userService: UserService) {}
 
-    @Roles(Role.Admin)
+    
     @Post()
     async create(@Body() data: CreateUserDTO) {
         return this.userService.create(data);
     }
 
-    @Roles(Role.Admin)
     @Get()
     async listUsers() {
         return this.userService.listUsers()
     }
 
-    @Roles(Role.Admin)
     @Get(':id')
     async listUserId(@ParamId() id: number) {
         console.log({id})
         return this.userService.listUserId(id)
     }
 
-    @Roles(Role.Admin)
     @Put(':id')
     async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
             return this.userService.update(id, data)
     }
 
-    @Roles(Role.Admin)
     @Patch(':id')
     async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number) {
         return this.userService.updatePartial(id, data)
     }
 
-    @Roles(Role.Admin)
     @Delete(':id')
     async delete(@ParamId() id: number) {
         return this.userService.delete(id)
