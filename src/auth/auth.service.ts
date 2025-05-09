c2fb2ed090bc27f0cdbe211ff8f4ab29a26f14e8
JeerTeles <jadersoncontatos@gmail.com>
2025-05-08 23:15:46 -0300

    feature: create auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,59 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { users } from "generated/prisma";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthResgisterDTO } from "./dto/auth-register.dto";
+import { UserService } from "src/user/user.service";
 
 @Injectable()
 export class AuthService {
+
+    private issuer = 'login'
+    private audience = 'users'
     
     constructor(
         private readonly jwtService: JwtService, 
-        private readonly prisma: PrismaService
+        private readonly prisma: PrismaService,
+        private readonly userService: UserService
     ) {}
 
-    async createToken() {
-        //return this.jwtService.sign()
+    createToken(user: users) {
+        return {
+            accessToken: this.jwtService.sign({
+                id: user.id,
+                name: user.name,
+                email: user.email
+            },  {
+                expiresIn: "7 days",
+                subject: String(user.id),
+                issuer: this.issuer,
+                audience: this.audience,
+                //notBefore: Math.ceil((Date.now() + 1000 * 60 * 60) / 1000)  [Ativação do token apos uma hora]
+            })
+        }
     }
 
-    async checkToken(token: string) {
-        //return this.jwtService.verify()
+    checkToken(token: string) {
+
+        try {
+            const data =  this.jwtService.verify(token, {
+                issuer: this.issuer,
+                audience: this.audience,
+            })
+
+            return data
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+    }
+
+    isValidToken(token: string) {
+        try {
+            this.checkToken(token)
+            return true
+        } catch (e) {
+            return false
+        }
     }
 
     async login(email: string, password: string) {
@@ -31,7 +69,7 @@ export class AuthService {
             throw new  UnauthorizedException('Incorrect email and/or password')
         }
 
-        return user
+        return this.createToken(user)
     }
 
     async forget(email: string) {
@@ -51,7 +89,8 @@ export class AuthService {
     async reset(password: string, token: string) {
         // TO DO: Vaidar o token...
         const id = 0
-        await this.prisma.users.update({
+        
+        const user = await this.prisma.users.update({
             where: {
                 id,
             },
@@ -60,6 +99,12 @@ export class AuthService {
             }
         })
 
-        return true
+        return this.createToken(user)
+    }
+
+    async register (data: AuthResgisterDTO) {
+        const user = await this.userService.create(data)
+
+        return this.createToken(user)
     }
 }
