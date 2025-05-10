f90d418420a0a3a5285ad36394b4e646d88accda
JeerTeles <jadersoncontatos@gmail.com>
2025-05-09 23:57:07 -0300

    feat: imports ConfigModule and ThrottlerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,27 @@
+import {APP_GUARD} from '@nestjs/core';
 import { forwardRef, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+     ttl: 60,
+     limit: 100,
+     // permitindo o acesso ao googlebot ignoreUserAgents: [/googlebot/gi]
+    }),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule) 
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD, 
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule {}
