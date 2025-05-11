46574f0e3b051712bd3258f197d46cccf1189adb
JeerTeles <jadersoncontatos@gmail.com>
2025-05-10 23:31:52 -0300

    feature: imports MailerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter} from '@nestjs-modules/mailer/dist/adapters/pug.adapter'
 
 @Module({
   imports: [
@@ -16,7 +18,29 @@ import { ConfigModule } from '@nestjs/config';
      // permitindo o acesso ao googlebot ignoreUserAgents: [/googlebot/gi]
     }),
     forwardRef(() => UserModule),
-    forwardRef(() => AuthModule) 
+    forwardRef(() => AuthModule),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: 'smtp.ethereal.email',
+          port: 587,
+          auth: {
+            user: 'rodrigo56@ethereal.email',
+            pass: '1HF4Wm88aDxg9tD62a'
+          }
+        },
+        defaults: {
+          from: '"JeerDev" <rodrigo56@ethereal.email',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }), 
   ],
   controllers: [AppController],
   providers: [AppService, {
